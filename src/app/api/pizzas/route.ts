9e417778fb5a8360ba65e05ba764c0e72bf28545
Bruno Santos Silva <jkbrunowk@hotmail.com>
2025-11-16 14:23:52 -0300

    fix: adiciona type Product no endpoint pizzas

--- src/app/api/pizzas/route.ts
+++ src/app/api/pizzas/route.ts
@@ -1,9 +1,10 @@
 import { getAllProducts } from "@/services/product";
 import { NextResponse } from "next/server";
+import { Product } from "@/generated/prisma";
 
 export async function GET() {
   let pizzas = await getAllProducts();
-  pizzas = pizzas.map((pizza) => ({
+  pizzas = pizzas.map((pizza: Product) => ({
     ...pizza,
     image: `${process.env.NEXT_PUBLIC_BASE_URL}/pizzas/${pizza.image}`,
   }));
